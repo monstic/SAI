39736223519fac14bb2f4f78156112f547a732df
monstic <monsticbr@gmail.com>
2017-06-01 20:04:18 -0300

    SAI

--- lib.sai.js
+++ lib.sai.js
@@ -1081,50 +1081,47 @@ function (room) {
 
 moveToByPath = 
 function (creep, target) {
-    if (!creep.memory.path || creep.memory.path === 'undefined') {
-        let roomName = creep.pos.roomName;
-        let ret = PathFinder.search(
-            creep.pos, target,
-            {
-            // We need to set the defaults costs higher so that we
-            // can set the road cost lower in `roomCallback`
-            plainCost: 2,
-            swampCost: 10,
+    let roomName = creep.pos.roomName;
+    let ret = PathFinder.search(
+        creep.pos, target,
+        {
+        // We need to set the defaults costs higher so that we
+        // can set the road cost lower in `roomCallback`
+        plainCost: 2,
+        swampCost: 10,
 
-            roomCallback: function(roomName) {
+        roomCallback: function(roomName) {
 
-                let room = Game.rooms[roomName];
-                // In this example `room` will always exist, but since 
-                // PathFinder supports searches which span multiple rooms 
-                // you should be careful!
-                if (!room) return;
-                let costs = new PathFinder.CostMatrix;
+            let room = Game.rooms[roomName];
+            // In this example `room` will always exist, but since 
+            // PathFinder supports searches which span multiple rooms 
+            // you should be careful!
+            if (!room) return;
+            let costs = new PathFinder.CostMatrix;
 
-                room.find(FIND_STRUCTURES).forEach(function(struct) {
-                if (struct.structureType === STRUCTURE_ROAD) {
-                    // Favor roads over plain tiles
-                    costs.set(struct.pos.x, struct.pos.y, 1);
-                } else if (struct.structureType !== STRUCTURE_CONTAINER &&
-                            (struct.structureType !== STRUCTURE_RAMPART ||
-                            !struct.my)) {
-                    // Can't walk through non-walkable buildings
-                    costs.set(struct.pos.x, struct.pos.y, 0xff);
-                }
-                });
-
-                // Avoid creeps in the room
-                room.find(FIND_CREEPS).forEach(function(creep) {
-                costs.set(creep.pos.x, creep.pos.y, 0xff);
-                });
-
-                return costs;
-            },
+            room.find(FIND_STRUCTURES).forEach(function(struct) {
+            if (struct.structureType === STRUCTURE_ROAD) {
+                // Favor roads over plain tiles
+                costs.set(struct.pos.x, struct.pos.y, 1);
+            } else if (struct.structureType !== STRUCTURE_CONTAINER &&
+                        (struct.structureType !== STRUCTURE_RAMPART ||
+                        !struct.my)) {
+                // Can't walk through non-walkable buildings
+                costs.set(struct.pos.x, struct.pos.y, 0xff);
             }
-        );
+            });
 
-        var pos = ret.path[0];
+            // Avoid creeps in the room
+            room.find(FIND_CREEPS).forEach(function(creep) {
+            costs.set(creep.pos.x, creep.pos.y, 0xff);
+            });
 
-    }
+            return costs;
+        },
+        }
+    );
+
+    var pos = ret.path[0];
 
     if (pos) {
         creep.move(creep.pos.getDirectionTo(pos));
