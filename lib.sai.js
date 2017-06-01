4e39bcb1a89ba36aef0cae8e177f95c383b6621b
monstic <monsticbr@gmail.com>
2017-05-31 22:16:14 -0300

    testing auto build walls

--- lib.sai.js
+++ lib.sai.js
@@ -855,7 +855,6 @@ function (room) {
 
     //PASSO 12 - CRIAR MUROS
     if (Memory.rooms[room.name].info.constructionslevel === 12) {
-        console.log('12');
         if (room.controller.level >= 5) {
             var constructionSites = room.find(FIND_CONSTRUCTION_SITES);
             if (constructionSites.length === 0) {
