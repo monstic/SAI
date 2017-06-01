5888ba872fadd86a8fb62a4ce6ebb5ec4ccc4ad9
monstic <monsticbr@gmail.com>
2017-06-01 16:50:04 -0300

    SAI

--- creeps.type.claimer.js
+++ creeps.type.claimer.js
@@ -2,7 +2,15 @@ var creepType = function(creep) {
 
     if (creep.memory.goto) {
         var flag = Game.flags.claim;
-        moveToByPath(creep, flag.pos);
+        if (creep.memory.goto !== creep.room.name) {
+            moveToByPath(creep, flag.pos);
+        }
+        else {
+            if (creep.claimController(creep.room.controller) === ERR_NOT_IN_RANGE) {
+                moveToByPath(creep, creep.room.controller.pos);
+            }
+            creep.claimController(Game.rooms.controller);
+        }
     }
 
 
