6f33b2cdc61ac6b2b3a497735890f2aaca35088c
monstic <monsticbr@gmail.com>
2017-05-31 23:34:54 -0300

    .

--- lib.sai.js
+++ lib.sai.js
@@ -922,7 +922,7 @@ function (room) {
                         place.createConstructionSite(STRUCTURE_WALL);
                     }
                     if (i === parseInt(countExits.length/2)) {
-                        var place = new RoomPosition(2, Memory.rooms[room.name].exit.left[exit], room.name);
+                        var place = new RoomPosition(2, (Memory.rooms[room.name].exit.left[exit]-1), room.name);
                         place.createConstructionSite(STRUCTURE_RAMPART);
                     }
                     if (i === countExits.length) {
