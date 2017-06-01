d3db5a53dd8b931f53b26f512348fbfa47f8731e
monstic <monsticbr@gmail.com>
2017-06-01 00:11:59 -0300

    trying fix left wall

--- lib.sai.js
+++ lib.sai.js
@@ -924,7 +924,7 @@ function (room) {
                         var place = new RoomPosition(2, (Memory.rooms[room.name].exit.left[exit]+1), room.name);
                         place.createConstructionSite(STRUCTURE_RAMPART);
                     }
-                    if (i === (countExits.length+1)) {
+                    if (i === (countExits.length)) {
                         var place = new RoomPosition(2, (Memory.rooms[room.name].exit.left[exit]+1), room.name);
                         place.createConstructionSite(STRUCTURE_WALL);
                         var place = new RoomPosition(2, (Memory.rooms[room.name].exit.left[exit]+2), room.name);
