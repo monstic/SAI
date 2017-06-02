5551589d89ba9c76cec3a9becb5328da852ee534
monstic <monsticbr@gmail.com>
2017-06-01 22:00:03 -0300

    SAI

--- creeps.action.claiming.js
+++ creeps.action.claiming.js
@@ -2,20 +2,30 @@ var creepActFunctions = function(creep) {
 
     //SET TARGET
     if (!creep.memory.targetId || creep.memory.targetId === 'undefined') {
-        cleanTarget(creep);
-        var flag = Game.flags.claim;
-        setTarget(creep, 'claim', 'CONTROLLER', flag.pos.roomName);
+        if (creep.room.name !== creep.memory.goto) {
+            cleanTarget(creep);
+            var flag = Game.flags.claim;
+            setTarget(creep, 'travel', 'FLAG', flag.pos.roomName);
+        }
+        else {
+            cleanTarget(creep);
+            setTarget(creep, 'claim', 'CONTROLLER', creep.pos.roomName);
+        }
     }
+
+       
 
     //CLAIM CONTROLLER
     if (creep.memory.targetId) {
-        var target = Game.flags.claim;
-        if (target.pos.roomName !== creep.room.name) {
-            moveToByPath(creep, target.pos);
-        }
-        else {
-            if(creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+        if (creep.memory.targetType === 'FLAG') {
+            var target = Game.flags.claim;
+            if (target.pos.roomName !== creep.room.name) {
                 moveToByPath(creep, target.pos);
+            }
+        }
+        if (creep.memory.targetType === 'CONTROLLER') {
+            if(creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                moveToByPath(creep, creep.room.controller);
             }
         }
     }
--- creeps.type.claimer.js
+++ creeps.type.claimer.js
@@ -3,10 +3,15 @@ var creepType = function(creep) {
     //DEFINE ACTIONS
     if (creep.memory.action === 'undefined') {
         creep.memory.action = 'claiming';
-        creep.say('claim');
+        creep.say('travel');
         cleanTarget(creep);
     }
-
+    if (creep.room.targetType === 'travel' && creep.room.name === creep.memory.goto) {
+        cleanTarget(creep);
+    }
+    if (creep.room.targetType === 'claim' && creep.room.name !== creep.memory.goto) {
+        cleanTarget(creep);
+    }
 
 };
 
