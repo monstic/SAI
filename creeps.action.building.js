bb6934056e13d761d83a1ace952b4edc684422d8
monstic <monsticbr@gmail.com>
2017-06-01 23:57:58 -0300

    SAI

--- creeps.action.building.js
+++ creeps.action.building.js
@@ -9,6 +9,7 @@ var creepActFunctions = function(creep) {
                 var flag = Game.flags.claim;
                 var flagPos = new RoomPosition(flag.pos.x, flag.pos.y, flag.pos.roomName);
                 var csspawn = flagPos.room.find(FIND_CONSTRUCTION_SITES);
+                console.log(csspawn);
                 if (csspawn.length > 0) {
                     setTarget(creep, csspawn.id, 'CTST', csspawn.pos.roomName);
                 }
