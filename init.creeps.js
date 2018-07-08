54b5e4a0c4148b5420dafc3376d00d34eeb743c0
hermann <hermann@189.41.213.69>
2018-07-08 11:00:15 -0300

    None

--- init.creeps.js
+++ init.creeps.js
@@ -1,24 +1,3 @@
 module.exports = function (creep) {
 
-    //LOAD MODULES
-    //saveTrail(creep);
-
-    //GO TO TARGET ROOM
-    //if (creep.memory.targetId) {
-    //    if (creep.room.name !== creep.memory.targetRoom) {
-    //        var target = Game.getObjectById(creep.memory.targetId);
-    //        if (target) {
-    //            moveToByPath(creep, target);
-    //        }
-    //    }
-    //}
-    //if (creep.memory.sourceId) {
-    //    if (creep.room.name !== creep.memory.sourceRoom) {
-    //        var source = Game.getObjectById(creep.memory.sourceId);
-    //        if (source) {
-    //            moveToByPath(creep, source);
-    //        }
-    //    }
-    //}
-
 };
